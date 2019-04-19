10cb28212f56b239a0b28eeb3e0abb4134b84584
Rhioda muthie <rhiodam@gmail.com>
2019-04-19 18:36:24 +0700

    007 styling index page

--- index.php
+++ index.php
@@ -12,24 +12,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Doodle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<!--<p>Hi</p>-->
 
-<div class="main-section">
-<!--    LOGO-->
-    <div class="logoContainer">
-        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
-    </div>
-<!--    SEARCH-->
-    <div class="searchContainer">
+<div class="wrapper indexPage">
 
-        <form action="search.php" method="GET">
-            <input type="text" name="term" class="searchBox" placeholder="Enter anything..">
-            <input type="submit" value="Search" class="searchButton">
+    <div class="main-section">
+        <!--    LOGO-->
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        </div>
+        <!--    SEARCH-->
+        <div class="searchContainer">
 
-        </form>
+            <form action="search.php" method="GET">
+                <input type="text" name="term" class="searchBox" placeholder="Enter anything..">
+                <input type="submit" value="Search" class="searchButton">
+
+            </form>
+        </div>
     </div>
 </div>
+
 </body>
 </html>
